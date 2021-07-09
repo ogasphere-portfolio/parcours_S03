02cf12d907c51917769174684c14b9bb31e9b3e0
Olivier <olivier@ogasphere.com>
2021-07-09 11:28:51 +0200

    Bonus algo Dichotomique à debugger

--- exos/js/bonus.js
+++ exos/js/bonus.js
@@ -17,23 +17,76 @@
  *      - on n'aura plus alors que 499 possibilités
  *    - et ainsi de suite jusqu'à tomber sur la bonne valeur
  *    - Habile non ?
- * 
+ *
  * Tu peux partir du code de l'exo6, mais on te suggère de repartir de 0 ;)
  * Tu l'auras constaté, c'est pas facile. C'est de l'algo. Mais en même temps, c'est un bonus, c'est fait pour se casser la tête ^^
  */
 
 // Attention, ici, le nom du module est "bonus" (et pas app, comme d'habitude, car c'est déjà pris ;))
 const bonus = {
-  init: function() {
-    console.log('bonus.init');
+  init: function () {
+    console.log("bonus.init");
 
     // On lance le jeu
     game.init();
-
+    bonus.createArray();
+    //bonus.play()
     // TODO
-  }
-}
+  },
+  play: function () {
+    if (game.guess(index) == "win") {
+      // si le nombre à été trouvé on arrete la partie
+      //break;
+    } else {
+      // Sinon on essaye un autre nombre
+      game.guess(index);
+    }
+  },
+  createArray: function () {
+    let myListSort = [];
+    for (let index = 0; index < 2000; index++) {
+      myListSort.push(index + 1);
+    }
 
-document.addEventListener('DOMContentLoaded', bonus.init);
+    let result = bonus.searchDichotomiqueRécursive(game.secret, myListSort);
+    console.log(result);
+  },
 
+  searchDichotomiqueRécursive: function (numberSecret, myListSort) {
+    console.log("Nombre à trouvé" + numberSecret);
 
+    let len = myListSort.length;
+
+    console.log(len);
+    let m = Math.round(len / 2);
+    console.log(m);
+
+    if (myListSort[m] == numberSecret) {
+      console.log("result : " + m);
+      return m;
+    }
+
+    if (myListSort[m] > numberSecret) {
+      console.log("plus grand" + myListSort[m]);
+      myListSort.length = 0;
+      for (let index = 0; index < m - 1; index++) {
+        myListSort.push(index + 1);
+      }
+      console.log("nouvelle longueur si plus grand" + myListSort.length);
+
+      return bonus.searchDichotomiqueRécursive(numberSecret, myListSort);
+    } else {
+      console.log("plus petit" + myListSort[m]);
+
+      myListSort.length = 0;
+      for (let index = m + 1; index < len; index--) {
+        myListSort.push(index);
+      }
+      console.log("nouvelle longueur si plus petit" + myListSort.length);
+
+      return bonus.searchDichotomiqueRécursive(numberSecret, myListSort);
+    }
+  },
+};
+
+document.addEventListener("DOMContentLoaded", bonus.init);
