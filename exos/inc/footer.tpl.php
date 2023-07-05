39116090e56e62a145fbdad03a0e68bddf31c5bd
Olivier340 <46643169+Olivier340@users.noreply.github.com>
2023-07-05 07:25:17 +0200

    add readme.html

--- exos/inc/footer.tpl.php
+++ exos/inc/footer.tpl.php
@@ -1,11 +1,8 @@
 
     </main>
     <footer id="footer">
-        <p>
-            <strong>Besoin d'aide ?</strong> Direction les
-            <a href="https://kourou.oclock.io/ressources/fiches-recap/"><i>📚</i> Fiches recap</a>
-        </p>
-        <p>O'clock</p>
+        
+        <p>Ogasphere</p>
     </footer>
     <script src="<?= getAbsoluteUrl() ?>js/app.js"></script>
 </body>
